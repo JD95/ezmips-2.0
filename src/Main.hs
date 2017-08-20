680d23fe7459b1ae24811bef140a4e7c976a20fb
Jeff <jeffreydwyer95@hotmail.com>
2017-08-19 17:35:10 -0700

    setup basic alex and happy parsers

---
+++ src/Main.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Prelude ()
+import Protolude
+
+import Data.Semigroup ((<>))
+import Options.Applicative
+import Lens.Micro.Platform
+
+import qualified Lexer.Token as T
+import qualified Parser.Grammar as G
+
+data Options = Options { message :: Text }
+
+opts :: Parser Options
+opts = Options <$> option auto
+     ( long "message"
+    <> short 'm'
+    <> metavar "MESSAGE"
+    <> help "The message to print"
+    <> showDefault
+    <> value "Hello, World!"
+      )
+
+optsInfo :: ParserInfo Options
+optsInfo = info (opts <**> helper)
+  ( fullDesc
+  <> progDesc "Print a simple message"
+  <> header "ezmips - a minimal application"
+  )
+
+main :: IO ()
+main = do
+  options <- execParser optsInfo
+  putStrLn (message options)
+
